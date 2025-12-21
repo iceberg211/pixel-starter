59e4db6608c5fb890d20c21eccdc8371fae1f31e
weihe <187399692@qq.com>
2025-12-22 00:12:32 +0800

    feat: 启用并配置R2作为增量缓存

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
-  // 使用默认配置
-  // 可选：配置 R2 增量缓存
-  // incrementalCache: r2IncrementalCache,
+  // 使用 R2 作为增量缓存
+  incrementalCache: r2IncrementalCache,
 });
